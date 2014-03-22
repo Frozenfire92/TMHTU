6094e7ecfac1a0432bf2e947b9afad048544f49f
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-03-21 23:38:59 -0300

    baseline

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,14 +13,16 @@ public class PlayerController : MonoBehaviour
 	public float tilt;				//speed of the tilt
 	public Boundary bounds; 		//boundary class to lock the player in the game world
 
+	public float rotationSpeed;
+
 	public GameObject bullet; 		//reference to the bullet prefab
 	public Transform bulletSpawn; 	//reference to where the bullet will spawn
 	public float fireRate = 0.5f; 	//the time in seconds between shots
 
 	private float nextFireTime;		//keeps track of when the user can shoot again
 
-	// Update is called once per frame
-	void Update () 
+
+	void Update() 
 	{
 		//If fire button is pressed and we are past the nextFireTime, shoot a bullet
 		if (Input.GetButton("Fire1") && Time.time > nextFireTime)
@@ -28,18 +30,30 @@ public class PlayerController : MonoBehaviour
 			nextFireTime = Time.time + fireRate;
 			Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
 		}
+
+		//If Horizontal axis, rotate ship
+		//float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		//rotation *= Time.deltaTime; 
+		//transform.Rotate(0, rotation, 0);
 	}
+
 
 	//Updates based on physics ticks
 	void FixedUpdate()
 	{
 		//Get user input
-		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
-
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		
 		//Apply input to a Vector3, then apply that vector3 to the ships velocity
 		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
 		rigidbody.velocity = movement * speed;
+
+		//Get user input
+		//if (Input.GetButton("Accelleration"))
+		//{
+		//	rigidbody.AddForce(Vector3.forward * speed);
+		//}
 
 		//Lock the ship within the game bounds
 		rigidbody.position = new Vector3
@@ -50,6 +64,6 @@ public class PlayerController : MonoBehaviour
 			);
 
 		//Tilt the ship based on x velocity
-		rigidbody.rotation = Quaternion.Euler(0f, 0f, rigidbody.velocity.x * -tilt);
+		//rigidbody.rotation = Quaternion.Euler(0f, 0f, rigidbody.velocity.x * -tilt);
 	}
 }
